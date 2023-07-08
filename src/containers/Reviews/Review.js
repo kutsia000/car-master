9f2eda04b2e41085edaec15f3e05a0fdf923a4fa
giorgi_kutsia <giokucia1@gmail.com>
2023-07-08 19:48:08 +0400

    sss

--- src/containers/Reviews/Review.js
+++ src/containers/Reviews/Review.js
@@ -3,7 +3,18 @@ import { useTranslation } from 'react-i18next';
 import { AdminServiceContext } from '../../services/AdminService';
 
 const Review = () => {
-  return <>reviewPage</>;
+  const { getReview, review } = useContext(AdminServiceContext);
+  const { i18n } = useTranslation();
+  //console.log(getReviews);
+  const [loading, setLoading] = useState(true);
+  const lang = i18n.language || 'en';
+
+  return (
+    <>
+      <h2>reviewPage</h2>
+      
+    </>
+)
 };
 
 export default Review;
