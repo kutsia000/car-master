86fa7f8d3653c99ec87b45cfa5aa97ab32c2f269
Nino Kobaidze <ninokobaidze25@gmail.com>
2023-10-26 12:37:26 +0400

    dealer dialog

--- src/containers/Notification/NotificationDialog.js
+++ src/containers/Notification/NotificationDialog.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import '../../Main.css';
 import styles from '../../components/Dialog/Dialog.module.scss';
+import AppButton from '../../components/AppButton/AppButton';
 
 const NotificationDialog = ({ notification, handleAgree }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -19,17 +20,17 @@ const NotificationDialog = ({ notification, handleAgree }) => {
     }
   };
 
+  isOpen ? (document.body.style.overflow = 'hidden') : (document.body.style.overflow = '');
+
   return (
     <>
       {isOpen && notification && (
-        <div className={styles.Dialog} open>
+        <div className={styles.Dialog} style={{top: 0, left: 0}} open>
           <div className={styles.Dialog__content}>
             <div>
               <h2>{notification.title}</h2>
               <p>{notification.content}</p>
-              <button type="button" onClick={() => handleNotifAgree()}>
-                agree
-              </button>
+              <AppButton type={'button'} large label={'agree'} onClick={() => handleNotifAgree()}/>
             </div>
           </div>
         </div>
